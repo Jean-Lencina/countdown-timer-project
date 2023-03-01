65276fe501f2802380578c9d9c0f6ddaac4f13b8
Jean Lencina <110210742+Jean-Lencina@users.noreply.github.com>
2023-02-28 21:55:28 -0300

    Start a countdown project

---
+++ app.js
@@ -0,0 +1,29 @@
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+]
+
+const timetotravel = document.querySelector('.time-to-travel');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
