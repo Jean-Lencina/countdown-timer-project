7a0e3c91a502b31c72bd0687ccb2cec017dc79fd
Jean Lencina <110210742+Jean-Lencina@users.noreply.github.com>
2023-03-01 22:29:24 -0300

    continue countdown

--- app.js
+++ app.js
@@ -23,7 +23,20 @@ const weekdays = [
     "Saturday",
 ]
 
-const timetotravel = document.querySelector('.time-to-travel');
+const timetotravel = document.querySelector('.timetotravel');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
+let futureDate = new Date(2024, 1, 8, 8, 30, 00);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month]
+const date = futureDate.getDate()
+
+
+
+timetotravel.textContent = `We will travel to paris in ${date} ${month} ${year} ${hours}:${minutes}am`
