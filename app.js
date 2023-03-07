864298fdbcda6aefe4bf2189fd6e1268f027a404
Jean Lencina <110210742+Jean-Lencina@users.noreply.github.com>
2023-03-06 21:42:33 -0300

    Project finished

--- app.js
+++ app.js
@@ -47,15 +47,42 @@ const futureTime = futureDate.getTime()
 function getRemainingTime(){
     const today = new Date().getTime()
     const t = futureTime - today
-    console.log(t)
     // 1s = 1000ms
     // 1m = 60s
     // 1hr = 60m
     // 1d = 24hr
 
     // values in ms
-    const oneDay = 24*60*60*1000
-    console.log(oneDay)
-}
+    const oneDay = 24 * 60 * 60 * 1000
+    const oneHour = 60 * 60 * 1000
+    const oneMinute = 60 * 1000
 
+    // calculate all values
+    let days= t / oneDay
+    days = Math.floor(days)
+    let hours = Math.floor((t % oneDay) / oneHour)
+    let minutes = Math.floor((t % oneHour) / oneMinute)
+    let seconds = Math.floor((t % oneMinute) / 1000)
+
+    // set values array
+
+    const values = [days, hours, minutes, seconds]
+
+    function format(item){
+        if(item < 10){
+            return item = `0${item}`
+        }
+        return item
+    }
+
+    items.forEach(function(item, index){
+        item.innerHTML = format(values[index])
+    })
+    if(t < 0){
+        clearInterval(countdown)
+        deadline.innerHTML = `<h4 class="expired">Congrats! We probably already travelling right now</h4>`
+    }
+
+}
+let countdown = setInterval(getRemainingTime, 1000)
 getRemainingTime()
